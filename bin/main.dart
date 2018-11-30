b24ad5454f1a663c84643960e91d077d6e4e4f39
Mafinar K <mafinar@gmail.com>
2018-11-30 12:14:47 -0500

    Create structure with stagehand

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:project_euler_dart/project_euler_dart.dart' as project_euler_dart;
+
+main(List<String> arguments) {
+  print('Hello world: ${project_euler_dart.calculate()}!');
+}
---
+++ test/project_euler_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:project_euler_dart/project_euler_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
