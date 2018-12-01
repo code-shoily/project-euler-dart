2a29e401b0e54e2998ddbe0672c36a5451a515e1
Mafinar K <mafinar@gmail.com>
2018-11-30 20:07:14 -0500

    Solve problem 3

--- lib/problem_1_10.dart
+++ lib/problem_1_10.dart
@@ -31,3 +31,20 @@ problem_2() => (([butLast = 1, last = 1]) sync* {
     })()
         .where((i) => i % 2 == 0)
         .reduce((a, b) => a + b);
+
+/// The prime factors of 13195 are 5, 7, 13 and 29.
+///
+/// What is the largest prime factor of the number 600851475143 ?
+///
+/// Here's the [link](https://projecteuler.net/problem=2)
+problem_3() {
+  int n = 600851475143, i = 2;
+
+  while (i * i <= n)
+    if (n % i == 0)
+      n ~/= i;
+    else
+      i++;
+
+  return n;
+}
